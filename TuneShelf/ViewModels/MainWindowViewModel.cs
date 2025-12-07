1c366e118cfbe7e082c72864f0d3667e839deb66
winntale <wintale126@gmail.com>
2025-12-07 14:24:26 +0300

    mvvm configuring with window showing dummy data

--- TuneShelf/ViewModels/MainWindowViewModel.cs
+++ TuneShelf/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,50 @@
-﻿namespace TuneShelf.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using TuneShelf.Models;
 
-public partial class MainWindowViewModel : ViewModelBase
+namespace TuneShelf.ViewModels;
+
+public sealed class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private string _title = "TuneShelf - Music Library";
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (_title == value) return;
+            _title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<Track> Tracks { get; init; } = new();
+
+    public MainWindowViewModel()
+    {
+        SeedDummyData();
+    }
+
+    private void SeedDummyData()
+    {
+        Tracks.Add(new Track
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Track 1",
+            Duration = 180,
+            Genre = "Rock",
+            Rating = 4.5m,
+            AlbumId = Guid.Empty
+        });
+        
+        Tracks.Add(new Track
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test Track 2",
+            Duration = 240,
+            Genre = "Electronic",
+            Rating = 5.0m,
+            AlbumId = Guid.Empty
+        });
+    }
 }
--- TuneShelf/ViewModels/ViewModelBase.cs
+++ TuneShelf/ViewModels/ViewModelBase.cs
@@ -1,7 +1,14 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.ComponentModel; // PropertyChangedEventHandler
+using System.Runtime.CompilerServices; // CallerMemberName
 
 namespace TuneShelf.ViewModels;
 
-public abstract class ViewModelBase : ObservableObject
+public abstract class ViewModelBase : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
